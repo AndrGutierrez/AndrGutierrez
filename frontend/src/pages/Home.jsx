8b176188db8e95306bfe52b6541be7f6ba1fb1e9
Andrés <andresgutierrezdev@outlook.com>
2021-08-19 14:46:26 -0400

    initial setup

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container-fluid">
+      <h1 className="display-4">I'ts me!</h1>
+    </div>
+  );
+}
+export default Home;
---
+++ frontend/src/routes/App.js
@@ -0,0 +1,9 @@
+// import logo from '../logo.svg';
+import '../assets/styles/App.css';
+import Home from '../pages/Home';
+
+function App() {
+  return <Home></Home>;
+}
+
+export default App;
